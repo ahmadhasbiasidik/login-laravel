842ef5d04b0451f635d3e21d6cdf3d305deecc0f
ahmadhasbia <hahasata632@gmail.com>
2023-06-09 17:01:38 +0700

    Membuat view

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="generator" content="">
+    <title>Signin Template · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sign-in/">
+
+        <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="/css/signin.css" rel="stylesheet">
+</head>
+<body class="text-center">
+    
+    <main class="form-signin">
+      <form>
+          <h3>My App</h3>
+          <h1 class="h3 mb-3 fw-normal">Please login</h1>
+
+          <div class="form-floating">
+            <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
+            <label for="email">Email address</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" name="password" class="form-control" id="password" placeholder="Password">
+            <label for="password">Password</label>
+          </div>
+          
+          </div>
+          <button class="w-100 btn btn-lg btn-dark" type="submit">Login</button>
+          <p class="mt-5 mb-3 text-muted">&copy; 2023</p>
+      </form>
+    </main>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/dashboard', function () {
+    return view('/dashboard.index');
+});
+
+Route::get('/login', function () {
+    return view('login.index');
+});
+
+Route::get('/register', function () {
+    return view('login.register');
 });
